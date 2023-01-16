56b55bcf1e3577d533298fbbe42b2898e0339537
stereomon <rene.klatt@spryker.com>
2023-01-16 16:40:21 +0100

    APPS-6184 Fixed coverage.

---
+++ tests/SprykerSdkTest/SyncApi/OpenApi/Builder/ConsoleCommand/Arguments/GlueResourceMethodResponseArgumentsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdkTest\SyncApi\OpenApi\Builder\ConsoleCommand\Arguments;
+
+use Codeception\Test\Unit;
+use SprykerSdk\SyncApi\OpenApi\Builder\ConsoleCommand\Arguments\GlueResourceMethodResponseArguments;
+use SprykerSdk\SyncApi\SyncApiConfig;
+use SprykerSdkTest\SyncApi\SyncApiTester;
+
+/**
+ * @group SprykerSdkTest
+ * @group SyncApi
+ * @group OpenApi
+ * @group Builder
+ * @group ConsoleCommand
+ * @group Arguments
+ * @group GlueResourceMethodResponseArgumentsTest
+ */
+class GlueResourceMethodResponseArgumentsTest extends Unit
+{
+    /**
+     * @var \SprykerSdkTest\SyncApi\SyncApiTester
+     */
+    protected SyncApiTester $tester;
+
+    /**
+     * Tests that ensures we get a path to where this SDK is installed. Usually only when used within the SprykerSDK.
+     *
+     * @return void
+     */
+    public function testGetConsoleCommandArgumentsOnlyReturnsNotNullProperties(): void
+    {
+        // Arrange
+        $glueResourceMethodResonseArguments = new GlueResourceMethodResponseArguments();
+
+        // Act
+        $consoleArguments = $glueResourceMethodResonseArguments->getConsoleCommandArguments();
+
+        // Assert
+        $this->assertTrue(in_array('--apiType', $consoleArguments));
+        $this->assertFalse(in_array('--module', $consoleArguments));
+    }
+}
