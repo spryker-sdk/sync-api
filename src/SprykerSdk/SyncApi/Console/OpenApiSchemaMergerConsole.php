329cc19e2e54633119dba2b1bf283af1cac80fbd
René Klatt <rene.klatt@spryker.com>
2024-11-20 08:40:28 +0100

    ACP-4403 Updated schema merger to be configurable from console

--- src/SprykerSdk/SyncApi/Console/OpenApiSchemaMergerConsole.php
+++ src/SprykerSdk/SyncApi/Console/OpenApiSchemaMergerConsole.php
@@ -10,7 +10,9 @@ namespace SprykerSdk\SyncApi\Console;
 use cebe\openapi\spec\OpenApi;
 use cebe\openapi\Writer;
 use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 
@@ -35,51 +37,51 @@ class OpenApiSchemaMergerConsole extends Console
     {
         $this->setName(static::COMMAND_NAME);
         $this->setDescription(static::DESCRIPTION);
+
+        $this->addArgument('source', InputArgument::REQUIRED, 'The root file of the OpenApi schema which will be used to merge with other schemas.');
+        $this->addArgument('target', InputArgument::REQUIRED, 'The target file name that should be created after merge.');
+
+        $this->addOption('additional-schemas', 'a', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'The additional OpenApi schema files that should be merged with the root schema.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $projectSchemaPath = APPLICATION_ROOT_DIR . '/config/app/payone/api/openapi/payone.yml';
-        $projectSchemaPath = APPLICATION_ROOT_DIR . '/config/app/payone/api/openapi/payone-full.yml';
-        $generatedSchemaPath = APPLICATION_ROOT_DIR . '/resources/api/openapi.yml';
+        $source = sprintf('%s/%s', getcwd(), $input->getArgument('source'));
+        $target = sprintf('%s/%s', getcwd(), $input->getArgument('target'));
 
-        $coreSchemaPaths = [
-//            APPLICATION_ROOT_DIR . '/vendor/spryker/app-payment/resources/api/openapi.yml',
-//            APPLICATION_ROOT_DIR . '/vendor/spryker/app-kernel/resources/api/openapi.yml',
-            APPLICATION_ROOT_DIR . '/config/app/bazaarvoice/api/openapi/bazaarvoice.yml',
-            APPLICATION_ROOT_DIR . '/config/app/usercentrics/api/openapi/usercentrics.yml',
-        ];
+        $additionalSchemas = $input->getOption('additional-schemas');
 
         $yaml = new Yaml();
-        $projectSchema = $yaml->parseFile($projectSchemaPath);
+        $sourceSchema = $yaml->parseFile($source);
 
-        $paths = $projectSchema['paths'] ?? [];
-        $schemas = $projectSchema['components']['schemas'] ?? [];
-        $parameters = $projectSchema['components']['parameters'] ?? [];
+        $paths = $sourceSchema['paths'] ?? [];
+        $schemas = $sourceSchema['components']['schemas'] ?? [];
+        $parameters = $sourceSchema['components']['parameters'] ?? [];
 
-        foreach ($coreSchemaPaths as $item) {
-            $coreSchema = $yaml->parseFile($item);
+        foreach ($additionalSchemas as $additionalSchema) {
+            $additionalSchema = sprintf('%s/%s', getcwd(), $additionalSchema);
+            $coreSchema = $yaml->parseFile($additionalSchema);
             $paths = $this->recursiveMerge($coreSchema['paths'] ?? [], $paths);
             $schemas = $this->recursiveMerge($coreSchema['components']['schemas'] ?? [], $schemas);
             $parameters = $this->recursiveMerge($coreSchema['components']['parameters'] ?? [], $parameters);
         }
 
         $mergedOpenApi = new OpenApi([
-            'openapi' => $projectSchema['openapi'],
-            'info' => $projectSchema['info'],
-            'servers' => $projectSchema['servers'],
+            'openapi' => $sourceSchema['openapi'],
+            'info' => $sourceSchema['info'],
+            'servers' => $sourceSchema['servers'],
             'paths' => $paths,
             'components' => [
                 'schemas' => $schemas,
                 'parameters' => $parameters,
             ],
-            'security' => $projectSchema['security'] ?? [],
-            'tags' => $projectSchema['tags'] ?? [],
-            'externalDocs' => $projectSchema['externalDocs'] ?? [],
+            'security' => $sourceSchema['security'] ?? [],
+            'tags' => $sourceSchema['tags'] ?? [],
+            'externalDocs' => $sourceSchema['externalDocs'] ?? [],
         ]);
 
         // Save the merged specification to a file
-        Writer::writeToYamlFile($mergedOpenApi, $generatedSchemaPath);
+        Writer::writeToYamlFile($mergedOpenApi, $target);
 
         return static::CODE_SUCCESS;
     }
